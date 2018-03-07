839cdae863f68aabd10d17e6ce8dde9a98e757a3
나윤수 <skennel2@gmail.com>
2018-03-07 21:09:06 +0900

    test

--- src/test/java/org/almansa/web/PostServiceTest.java
+++ src/test/java/org/almansa/web/PostServiceTest.java
@@ -24,7 +24,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/root-context.xml", "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
-public class PostServiceTest {
+public class PostServiceTest {df 
 
 	@Autowired
 	PostService service;
