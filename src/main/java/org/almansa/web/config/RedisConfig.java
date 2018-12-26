afe413a7ad017d6fe122e2d74f599f1ba140fa96
skennel2 <skennel2@gmail.com>
2018-12-26 22:40:17 +0900

    Redis

--- src/main/java/org/almansa/web/config/RedisConfig.java
+++ src/main/java/org/almansa/web/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package org.almansa.web.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -9,13 +10,14 @@ import org.springframework.session.data.redis.config.annotation.web.server.Enabl
 // @EnableRedisHttpSession 어노테이션이 SpringSessionRepositoryFilter(Filter의 구현체) 라는 빈을 생성합니다
 // 이 필터는 HttpSession의 구현체를 바꾸는 역할을 합니다.  
 @EnableRedisWebSession
+@Configuration
 public class RedisConfig {
 	@Bean
-	public RedisConnectionFactory connectionFactory() {
+	public JedisConnectionFactory connectionFactory() {
 		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration("localhost", 6379);
 		
 		JedisConnectionFactory connectionFactory = new JedisConnectionFactory(redisConfig);
 		return connectionFactory;
 	}
-
+	
 }
